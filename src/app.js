3d239bd609c4cf39bbcb88b9c383848d53964dbb
Thiago Nascimento <thiago.nascimento8512@gmail.com>
2019-07-03 16:38:14 -0300

    Finalizado Criação de Meetup e Upload de Imagens

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { resolve } from 'path';
+import multer from 'multer';
 
 import routes from './routes';
 
@@ -9,14 +11,29 @@ class App {
     this.server = express();
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
     this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use((error, req, res, next) => {
+      if (error instanceof multer.MulterError) {
+        return res.status(500).json({ error: error.message });
+      }
+
+      return res.status(500).json({ error });
+    });
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import MeetupController from './app/controllers/MeetupController';
+
 import authMiddleware from './app/middlewares/auth';
+
+const upload = multer(multerConfig);
 
 const routes = new Router();
 
@@ -10,8 +18,18 @@ routes.get('/', (req, res) => {
   res.json({ msg: 'Hello World' });
 });
 
+// Rotas sem Autenticação
 routes.post('/users', UserController.store);
-routes.put('/users', authMiddleware, UserController.update);
 routes.post('/session', SessionController.store);
+
+// Rotas que necessitam de autenticação
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/meetup', MeetupController.store);
+// routes.put('/meetup', MeetupController.update);
 
 export default routes;
